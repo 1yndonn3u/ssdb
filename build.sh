61cb59d7a2d2d0b62aaf44bbf9d38371793d184a
deatheyes <deatheye@yeah.net>
2017-05-18 13:23:52 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BASE_DIR=`pwd`
-JEMALLOC_PATH="$BASE_DIR/deps/jemalloc-3.3.1"
+JEMALLOC_PATH="$BASE_DIR/deps/jemalloc-4.1.0"
 SNAPPY_PATH="$BASE_DIR/deps/snappy-1.1.4"
 ROCKSDB_PATH="$BASE_DIR/deps/rocksdb-5.3.3"
 
